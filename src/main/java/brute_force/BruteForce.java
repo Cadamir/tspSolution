faac330222c54be00a42eeb6ee2aeeb701d0d0a1
DJJMMusik <jus.mohr.19@lehre.mosbach.dhbw.de>
2022-04-29 16:56:21 +0200

    distance between 2 nodes now in Node.java

--- src/main/java/brute_force/BruteForce.java
+++ src/main/java/brute_force/BruteForce.java
@@ -3,11 +3,13 @@ package brute_force;
 import util.Node;
 import util.TspConverter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class BruteForce {
-    private Node[] nodes= new Node[280];
+    private final ArrayList<Node> nodes;
     private double best = 2000000;
 
     public BruteForce(String file) {
@@ -27,20 +29,20 @@ public class BruteForce {
         return best;
     }
 
-    public double calculatePathLength(Node[] nodes) {
+    public double calculatePathLength(ArrayList<Node> nodes) {
         double distance = 0;
         int i = 0;
-        while(i < nodes.length- 1) {
-            distance = distance + nodeDistance(nodes[i], nodes[i+1]);
+        while(i < nodes.size() - 1) {
+            distance = distance + nodes.get(i).distance(nodes.get(i+1));
             i++;
         }
-        distance = distance + nodeDistance(nodes[i], nodes[0]); // last node back to first node
+        distance = distance + nodes.get(i).distance(nodes.get(0)); // last node back to first node
         return distance;
     }
 
-    public double nodeDistance(Node node1, Node node2) {
-        return Math.round(Math.abs(Math.sqrt(
-                (node1.pos().x() - node2.pos().x()) * (node1.pos().x() - node2.pos().x())
-                        + ((node1.pos().y() - node2.pos().y()) * (node1.pos().y() - node2.pos().y())))));
+    public ArrayList<Node> shuffleNodes(ArrayList<Node> nodes) {
+        ArrayList<Node> route = new ArrayList<>(nodes);
+        Collections.shuffle(route);
+        return route;
     }
 }
--- src/main/java/util/Node.java
+++ src/main/java/util/Node.java
@@ -2,4 +2,13 @@ package util;
 
 //A Node holds a position of the tsp Problem mapped to its number
 public record Node(int nr, NodePosition pos) {
+    public double distance(Node node) {
+        return Math.round(Math.abs(Math.sqrt(
+                (
+                        this.pos().x() - node.pos().x()) * (this.pos().x() - node.pos().x()
+                ) + (
+                        this.pos().y() - node.pos().y()) * (this.pos().y() - node.pos().y()
+                )
+        )));
+    }
 }
