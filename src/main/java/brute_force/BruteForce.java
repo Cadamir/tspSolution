97c333f6ade062b62a90d7b4bf24ea79a142fc90
DJJMMusik <jus.mohr.19@lehre.mosbach.dhbw.de>
2022-04-29 15:46:41 +0200

    deleted not nessasary and partly deadlock danger removed

--- src/main/java/brute_force/BruteForce.java
+++ src/main/java/brute_force/BruteForce.java
@@ -3,23 +3,44 @@ package brute_force;
 import util.Node;
 import util.TspConverter;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class BruteForce {
     private Node[] nodes= new Node[280];
-    private int best = 2000000;
+    private double best = 2000000;
 
     public BruteForce(String file) {
         nodes = new TspConverter().generateFromFile(file);
-
     }
 
-    /*public BruteForce() throws FileNotFoundException {
-        BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Users\\Programmierer\\Documents\\unfinished\\bee\\src\\main\\java\\tsp280")));
+    public double solveLinear(){
+        best = 2000000;
+        best = calculatePathLength(nodes);
+        return best;
     }
 
-    public ArrayList<Integer> addKoordinate(HashMap<Integer, Integer> rest, ArrayList<Integer> al){
-        if(rest.size() == 0){
-            return al;
+    public double solveRandom(int limit) {
+        best = 2000000;
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        return best;
+    }
+
+    public double calculatePathLength(Node[] nodes) {
+        double distance = 0;
+        int i = 0;
+        while(i < nodes.length- 1) {
+            distance = distance + nodeDistance(nodes[i], nodes[i+1]);
+            i++;
         }
-        Math.random()
-    }*/
+        distance = distance + nodeDistance(nodes[i], nodes[0]); // last node back to first node
+        return distance;
+    }
+
+    public double nodeDistance(Node node1, Node node2) {
+        return Math.round(Math.abs(Math.sqrt(
+                (node1.pos().x() - node2.pos().x()) * (node1.pos().x() - node2.pos().x())
+                        + ((node1.pos().y() - node2.pos().y()) * (node1.pos().y() - node2.pos().y())))));
+    }
 }
