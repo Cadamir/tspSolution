4ea3067e0a37fd273fd2d35dd2a80e4f36501bff
DJJMMusik <jus.mohr.19@lehre.mosbach.dhbw.de>
2022-05-02 12:05:03 +0200

    brute force mostly done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import brute_force.BruteForce;
 public class Main {
     public static void main(String[] args) {
         BruteForce bf = new BruteForce("tsp280");
-        System.out.println(bf.solveLinear());
+        System.out.println("Endergebnis: " + bf.solveRandom(1600000));
     }
 }
--- src/main/java/brute_force/BruteForce.java
+++ src/main/java/brute_force/BruteForce.java
@@ -5,35 +5,73 @@ import util.Route;
 import util.TspConverter;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class BruteForce {
     private final ArrayList<Node> nodes;
-    private double best = 2000000;
+    private double gBest;
+    private Route gBestRoute;
+    private int limitCounter;
+    private final Object bestMutex = new Object();
 
     public BruteForce(String file) {
         nodes = new TspConverter().generateFromFile(file);
+        gBest = Double.MAX_VALUE;
     }
 
     public double solveLinear(){
-        best = 2000000;
+        gBest = Double.MAX_VALUE;
         Route route = new Route(nodes);
-        best = route.length();
-        return best;
+        gBest = route.length();
+        return gBest;
     }
 
     public double solveRandom(int limit) {
-        best = 2000000;
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        gBest = Double.MAX_VALUE;
+        int threadsCount = Math.min(Runtime.getRuntime().availableProcessors(), limit);
+        limitCounter = limit / threadsCount;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadsCount);
 
-            /* Route route = new Route(nodes);
-             * route.shuffle();
-             * route.length();
-             *
-             */
+        for (int i = 0 ; i < threadsCount ; i++) {
+            executorService.submit(this::solveRandomPermutations);
+        }
 
-        return best;
+        executorService.shutdown();
+        try {
+            boolean terminated = executorService.awaitTermination(10, TimeUnit.MINUTES); //maximal runtime
+            if (!terminated) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("BruteForce got canceled");
+        }
+
+
+        return gBest;
+    }
+
+    private void solveRandomPermutations() {
+        int counter = 0;
+        double pBest = Double.MAX_VALUE;
+        Route pBestRoute = null;
+        while (counter < limitCounter) {
+            Route route = new Route(nodes);
+            route.shuffle();
+            double length = route.length();
+            if (length < pBest) {
+                pBest = length;
+                pBestRoute = route;
+            }
+            counter++;
+        }
+        synchronized (bestMutex) {
+            if (pBest < gBest) {
+                System.out.println(gBest + " g <- " + pBest);
+                gBest = pBest;
+                gBestRoute = pBestRoute;
+            }
+        }
     }
 }
