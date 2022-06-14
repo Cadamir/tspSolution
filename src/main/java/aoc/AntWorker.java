b79f90170f02263e1d8859ce4fdea459d3eaec91
DJJMMusik <72066315+DJJMMusik@users.noreply.github.com>
2022-06-14 22:44:20 +0200

    sry bin noch nicht so weit

--- src/main/java/aoc/AntWorker.java
+++ src/main/java/aoc/AntWorker.java
@@ -1,11 +1,17 @@
 package aoc;
 
+import configuration.Configuration;
+import util.Node;
+
+import java.util.ArrayList;
 import java.util.concurrent.BrokenBarrierException;
 
 import static aoc.AntColonyOptimization.*;
 
 public class AntWorker {
 
+    static ArrayList<Node> stations;
+    static Pheromone[][] pheromones;
     public AntWorker(){
     }
     
@@ -24,24 +30,78 @@ public class AntWorker {
         
     }
 
-    private void best() {
-        //M
+    private void move() {
+        while(true) {
+            int antNr = toMove.decrementAndGet(); //TODO check - returns the actual value or the decremented value
+            if (antNr < 0) return;
+            Ant ant = ants[antNr];
+            for (int i = 0; i < stations.size() - 1; i++) {
+                ant.visitCity(selectNextCity(ant));//distance jeweils übergeben ? für schnellere Berechnung
+            }
+        }
     }
+
+    private Node selectNextCity(Ant ant) {
+        //random city
+        if (Configuration.INSTANCE.randomGenerator.nextDouble() < Configuration.INSTANCE.randomFactor) {
+            int t = Configuration.INSTANCE.randomGenerator.nextInt(stations.size());
+            if (!ant.visited(t)) {
+                return stations.get(t);
+            }
+        }
+
+        double[] probabilities = calculateProbabilities(ant);
+
+        double total2 = 0;
+        for (int i = 0; i < stations.size(); i++) {
+            total2 += probabilities[i];
+        }
+
+        double randomNumber = total2 * Configuration.INSTANCE.randomGenerator.nextDouble();
+
+        double total = 0;
+
+        for (int i = 0; i < stations.size(); i++) {
+            total += probabilities[i];
+            if (total >= randomNumber) {
+                return stations.get(i);
+            }
+        }
+
+        throw new RuntimeException("runtime exception | other cities");
+    }
+
+    public double[] calculateProbabilities(Ant ant) {
+        Node currentStation = ant.getRoute().getRoute().get(ant.getRoute().getRoute().size());
+        double pheromone = 0.01;
+
+        for (Node station : stations) {
+            if (!ant.visited(station.nr())) {
+                if(trails[currentStation.nr()][station.nr()] == 0)
+                    trails[currentStation.nr()][station.nr()] = Configuration.INSTANCE.initialPheromoneValue;
+                pheromone += Math.pow(trails[currentStation.nr()][station.nr()], Configuration.INSTANCE.alpha) * Math.pow(1.0 / distMatrix.get(currentStation.nr(),station.nr()), Configuration.INSTANCE.beta);
+            }
+        }
+
+
+        for (Node station : stations) {
+            if (ant.visited(station.nr())) {
+                probabilities[station.nr()] = 0.0;
+            } else {
+                double numerator = Math.pow(trails[currentStation.nr()][station.nr()], Configuration.INSTANCE.alpha) * Math.pow(1.0 / distMatrix.get(currentStation.nr(),station.nr()), Configuration.INSTANCE.beta);
+                probabilities[station.nr()] = numerator / pheromone;
+            }
+        }
+    }
+
+
 
     private void pheromon() {
         //U
     }
 
-    private void move() {
-        while(true) {
-            int antNr = toMove.decrementAndGet(); //TODO check - returns the actual value or the decremented value
-            if (antNr < 0) return;
-            AntWorker antWorker = antWorkers[antNr];
-            for (int i = 0; i < graph.length - 1; i++) {
-                antWorker.visitCity(currentIndex, selectNextCity(antWorker));
-            }
-
-            //M
-        }
+    private void best() {
+        //M
     }
+
 }
