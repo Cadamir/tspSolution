03302ca9c2ad07dac5ca800bdc58b13e63775038
DJJMMusik <jus.mohr.19@lehre.mosbach.dhbw.de>
2022-06-19 13:53:23 +0200

    working on commandline programm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,58 @@
 import aoc.AntColonyOptimization;
+import aoc.AntWorker;
 import configuration.Configuration;
 import util.Route;
 
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args){
-        long starttime = System.currentTimeMillis();
-        Configuration.INSTANCE.logOn = true;
-        aco();
-        long endtime = System.currentTimeMillis();
-        System.out.println(endtime-starttime);
+        String method = "aco";
+        URL log = AntWorker.class.getResource("../aocLog.log");
+        for (String para: args) {
+            String[] paras = para.split("=");
+            String key = paras[0].substring(1);
+            String value = paras[1];
+            switch (key) {
+                case "method":
+                    method = value.toLowerCase();
+                    break;
+                case "best":
+                    break;
+                case "save":
+                    break;
+                case "log":
+                    break;
+            }
+        }
+
+        switch (method) {
+            case "aco":
+                Configuration.INSTANCE.logOn = true;
+                long startTime = System.currentTimeMillis();
+                aco(log);
+                long endTime = System.currentTimeMillis();
+                System.out.println("The Search took " + ((endTime-startTime)/1000) + " seconds");
+                break;
+            case "bf":
+                break;
+            case "opt":
+                break;
+            default:
+                System.out.println("Undefined Method: '" + method + "'");
+                System.out.println("Please chose from the following method-parameters:");
+                System.out.println("aco ; bf ; opt");
+                System.out.println("example: -method=aco");
+                System.exit(0);
+        }
+
+
+
+
     }
 
-    public static void aco() {
-        AntColonyOptimization aco = new AntColonyOptimization("tsp280");
+    public static void aco(URL logResource) {
+        AntColonyOptimization aco = new AntColonyOptimization("tsp280", logResource);
         Route gBest = aco.solve();
         System.out.println("Best result is " + gBest.getLength() + " for the route :" + gBest.routeToString());
     }
